a9881a55298f15f5d658381fc87951b59f964176
aakib patel <akibgordhan@gmail.com>
2024-10-07 22:47:37 -0700

    first commit

---
+++ 167.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	printf("---------------\n");
+	printf("|             |\n");
+	printf("|5*5=25       |\n");
+	printf("|             |\n");
+	printf("---------------\n");
+}
